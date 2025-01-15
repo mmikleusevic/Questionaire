90ee5648e212a15e04727a00d50d2f4e86d23b28
mmikleusevic <mikleusevicmarin@gmail.com>
2025-01-15 15:41:13 +0100

    Updated QuestionController.cs logic

--- Api/Controllers/QuestionController.cs
+++ Api/Controllers/QuestionController.cs
@@ -1,43 +1,63 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using QuestionaireApi.Models;
 
 namespace QuestionaireApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class QuestionController : ControllerBase
+public class QuestionController(QuestionaireDbContext context) : ControllerBase
 {
         [HttpGet]
-        public ActionResult<List<Question>> GetQuestions()
+        public async Task<ActionResult<List<Question>>> GetQuestions()
         {
-                //TODO: fix
-                return Ok(new List<Question>());
+                return Ok(await context.Questions.ToListAsync());
         }
 
         [HttpGet]
         [Route("{id:int}")]
-        public ActionResult<Question> GetQuestionById(int id)
+        public async Task<ActionResult<Question>> GetQuestionById(int id)
         {
-                return Ok(new Question());
+                Question question = await context.Questions.FindAsync(id);
+
+                if (question is null) return NotFound();
+                        
+                return Ok(question);
         }
 
         [HttpPost]
-        public ActionResult<Question> AddQuestion(Question? newQuestion)
+        public async Task<ActionResult<Question>> AddQuestion(Question? newQuestion)
         {
                 if (newQuestion is null) return BadRequest();
+                
+                context.Questions.Add(newQuestion);
+                await context.SaveChangesAsync();
                 
                 return CreatedAtAction(nameof(GetQuestionById), new { id = newQuestion.Id }, newQuestion);
         }
 
         [HttpPut("{id:int}")]
-        public IActionResult UpdateQuestion(int id, Question? updatedQuestion)
+        public async Task<IActionResult> UpdateQuestion(int id, Question? updatedQuestion)
         {
+                Question question = await context.Questions.FindAsync(id);
+                if (question is null) return NotFound();
+                
+                updatedQuestion.QuestionText = question.QuestionText;
+                
+                await context.SaveChangesAsync();
+                
                 return NoContent();
         }
 
         [HttpDelete("{id:int}")]
-        public IActionResult DeleteQuestion(int id)
+        public async Task<IActionResult> DeleteQuestion(int id)
         {
+                Question question = await context.Questions.FindAsync(id);
+                if (question is null) return NotFound();
+                
+                context.Questions.Remove(question);
+                await context.SaveChangesAsync();
+                
                 return NoContent();
         }
 }
