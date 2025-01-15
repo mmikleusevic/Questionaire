9cde562d12fd5c9b1c1af21c3be67af98f2e510c
mmikleusevic <mikleusevicmarin@gmail.com>
2025-01-15 13:04:00 +0100

    Added scalar package for manual testing api

--- Api/obj/Debug/net9.0/QuestionaireApi.AssemblyInfo.cs
+++ Api/obj/Debug/net9.0/QuestionaireApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuestionaireApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f61af4236327be1ea6073926ff4351379bba26a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuestionaireApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuestionaireApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
