6cd65b16520ea02dc7e4c7222c9f2fa6dda23576
mmikleusevic <mikleusevicmarin@gmail.com>
2025-02-08 11:36:55 +0100

    Added basic categories to Category table, made a migration for it, updated unity version

--- Api/Migrations/20250208103226_AddedCategoriesToDbTable.Designer.cs
+++ Api/Migrations/20250208103226_AddedCategoriesToDbTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuestionaireApi;
 
@@ -11,9 +12,11 @@ using QuestionaireApi;
 namespace QuestionaireApi.Migrations
 {
     [DbContext(typeof(QuestionaireDbContext))]
-    partial class QuestionaireDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250208103226_AddedCategoriesToDbTable")]
+    partial class AddedCategoriesToDbTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Api/Migrations/20250208103226_AddedCategoriesToDbTable.cs
@@ -0,0 +1,565 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace QuestionaireApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCategoriesToDbTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CategoryName",
+                value: "Chemistry");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "Biology", 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "Astronomy", 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "Earth Science", 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CategoryName",
+                value: "History");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CategoryName",
+                value: "Ancient History");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "Medieval History", 8 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "Modern History", 8 });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CategoryName", "ParentCategoryId" },
+                values: new object[,]
+                {
+                    { 12, "World War I", 8 },
+                    { 13, "World War II", 8 },
+                    { 14, "Geography", null },
+                    { 18, "Mathematics", null },
+                    { 22, "Sports", null },
+                    { 27, "Entertainment", null },
+                    { 32, "Literature", null },
+                    { 35, "Mythology", null },
+                    { 39, "Technology", null },
+                    { 43, "Food & Drinks", null },
+                    { 46, "Religion", null },
+                    { 15, "Countries & Capitals", 14 },
+                    { 16, "World Landmarks", 14 },
+                    { 17, "Maps & Flags", 14 },
+                    { 19, "Algebra", 18 },
+                    { 20, "Geometry", 18 },
+                    { 21, "Calculus", 18 },
+                    { 23, "Football", 22 },
+                    { 24, "Basketball", 22 },
+                    { 25, "Tennis", 22 },
+                    { 26, "Olympics", 22 },
+                    { 28, "Movies", 27 },
+                    { 29, "TV Shows", 27 },
+                    { 30, "Music", 27 },
+                    { 31, "Video Games", 27 },
+                    { 33, "Classic Books", 32 },
+                    { 34, "Modern Books", 32 },
+                    { 36, "Greek Mythology", 35 },
+                    { 37, "Norse Mythology", 35 },
+                    { 38, "Egyptian Mythology", 35 },
+                    { 40, "Computers", 39 },
+                    { 41, "Internet", 39 },
+                    { 42, "Artificial Intelligence", 39 },
+                    { 44, "Food & Drinks - Cuisine", 43 },
+                    { 45, "Food & Drinks - Beverages", 43 },
+                    { 47, "Christianity", 46 },
+                    { 48, "Islam", 46 },
+                    { 49, "Hinduism", 46 },
+                    { 50, "Buddhism", 46 },
+                    { 51, "Rock", 30 },
+                    { 52, "Pop", 30 },
+                    { 53, "Classical", 30 },
+                    { 54, "Jazz", 30 },
+                    { 55, "Hip Hop", 30 },
+                    { 56, "Country", 30 },
+                    { 57, "Metal", 30 },
+                    { 58, "Blues", 30 },
+                    { 59, "Reggae", 30 },
+                    { 60, "Electronic", 30 },
+                    { 61, "Folk", 30 },
+                    { 62, "Opera", 30 },
+                    { 63, "Movie Soundtracks", 30 },
+                    { 64, "Music Theory", 30 },
+                    { 65, "Comedy", 28 },
+                    { 66, "Horror", 28 },
+                    { 67, "Action", 28 },
+                    { 68, "Romance", 28 },
+                    { 69, "Animation", 28 },
+                    { 70, "Fantasy", 28 },
+                    { 71, "Adventure", 28 },
+                    { 72, "Drama", 28 },
+                    { 73, "Thriller", 28 },
+                    { 74, "Comedy", 28 },
+                    { 75, "Comedy", 29 },
+                    { 76, "Documentary", 29 },
+                    { 77, "Drama", 29 },
+                    { 78, "Reality TV", 29 },
+                    { 79, "Animation", 29 },
+                    { 80, "Sitcom", 29 },
+                    { 81, "Game show", 29 },
+                    { 82, "Sci-fi", 29 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 31);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 33);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 34);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 36);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 37);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 38);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 40);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 41);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 42);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 44);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 45);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 47);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 48);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 49);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 50);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 51);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 52);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 53);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 54);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 55);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 56);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 57);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 58);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 59);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 60);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 61);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 62);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 63);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 64);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 65);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 66);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 67);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 68);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 69);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 70);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 71);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 72);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 73);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 74);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 75);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 76);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 77);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 78);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 79);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 80);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 81);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 82);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 30);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 32);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 35);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 39);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 43);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 46);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 27);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CategoryName",
+                value: "Biology");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "History", null });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "Ancient History", 5 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "Modern History", 5 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CategoryName",
+                value: "Geography");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CategoryName",
+                value: "World Geography");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "Music", null });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "CategoryName", "ParentCategoryId" },
+                values: new object[] { "Sports", null });
+        }
+    }
+}
